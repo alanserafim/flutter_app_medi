461dc2097fbccf94b8b4ee079ac50a0a97c13441
alanserafim <alan.serafim@fatec.sp.gov.br>
2024-11-30 11:33:07 -0300

    screen schedule

--- lib/screens/schedule.dart
+++ lib/screens/schedule.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_medi/components/schedule_item.dart';
 
 class Schedule extends StatelessWidget {
   const Schedule({super.key});
@@ -6,18 +7,44 @@ class Schedule extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        child: Column(
-          children: [
-            Container(),
-            Container(),
-            Container(),
-            ListView()
-          ],
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: Text(
+          "Meus horários",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: (){}),
+      body: ListView(
+        children: [
+          SizedBox(
+            height: 150,
+            child: Icon(
+              Icons.medication_rounded,
+              size: 128,
+              color: Colors.blueAccent,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Remédios de hoje",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+              ),
+            ],
+          ),
+          ScheduleItem("Dipirona", "1 comprimido", "09:00"),
+          ScheduleItem("Losartana", "2 comprimids", "10:00"),
+          ScheduleItem("Omeprazol", "1 comprimido", "13:00"),
+          ScheduleItem("Losartana", "2 comprimids", "20:00"),
+          ScheduleItem("Rivotril", "1 comprimids", "23:00"),
+          SizedBox(height: 70),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
